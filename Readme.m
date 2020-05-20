f4022558b2a57f04c911a00aafae3d2881f5af72
Usagi <f880708@gmail.com>
2020-05-20 10:12:57 +0800

    Update Readme.m

--- Readme.m
+++ Readme.m
@@ -1,8 +1,12 @@
 % 2020/05/06  Create by:Fred Liu
 
 % 此專案建立深度學習風格轉換APP，利用APP快速生成影像
+% 需要安裝VGG19的模型
 % MATLAB專案
 
+
+
 % Deep learning Image Style Transfer APP,using APP to quickly generate images
+% It need to install VGG-19 Network
 % MATLAB Projct 
 
